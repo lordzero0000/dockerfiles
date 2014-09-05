95b0a332cec8ad2f46d15c3ea9c57012457c4118
Axel Quack <mail@axelquack.de>
2014-09-05 09:33:27 +0200

    Delete couchpotato.sh

--- couchpotato/couchpotato.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-############################################################
-# Shell script to start CouchPotato
-# AUTHOR: Axel Quack <mail@axelquack.de>
-# Version 0.1
-############################################################
-
-exec /sbin/setuser nobody python /opt/couchpotato/CouchPotato.py --config_file=/config/config.ini --data_dir=/config/data
